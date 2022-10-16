48e1a414b827f38cd58c76ddcc30318adba13a1f
Pro4TLZZ <github@pro4tlzz.com>
2022-10-16 23:37:43 +0100

    Update overseerrSyncDeletedPlexMedia.js
    more fix pagination logic (mostly)

--- overseerrSyncDeletedPlexMedia.js
+++ overseerrSyncDeletedPlexMedia.js
@@ -5,36 +5,38 @@
 
         async function listMedia() {
         
-        var mediaList = [];
-        const limit = 100;
-        var skip = 100;
-        var url = '/api/v1/media?take=' + limit ;
-        
-        while (url) {
+            var mediaList = [];
+            const limit = 100;
+            var skip = 100;
+            var url = '/api/v1/media?take=' + limit ;
             
-            const r = await fetch (url, {method: 'get'});
-            const res = await r.json();
-            var results = res.results;
-            mediaList = mediaList.concat(results);
-            const pageInfo = res.pageInfo;
-            const page = pageInfo.page;
-            const pages = pageInfo.pages;
-
-            if (page < pages) {
-            
-                skip += 100;
-                var url = '/api/v1/media?take=' + limit + '&skip=' + skip;
-
-            }
-            else { 
-
-                mediaList = mediaList.concat(results);
-                console.table(mediaList);
-                return mediaList; 
+            while (url) {
                 
+                const r = await fetch (url, {method: 'get'});
+                const res = await r.json();
+                var results = res.results;
+                mediaList = mediaList.concat(results);
+                const pageInfo = res.pageInfo;
+                const page = pageInfo.page;
+                const pages = pageInfo.pages;
+    
+                if (page < pages) {
+                
+                    skip += 100;
+                    var url = '/api/v1/media?take=' + limit + '&skip=' + skip;
+    
+                }
+                else { 
+    
+                    mediaList = mediaList.concat(results);
+                    console.table(mediaList);
+                    return mediaList; 
+                    
+                }
+        
             }
-
-    }
+            
+            return mediaList;
 
 }
 }
