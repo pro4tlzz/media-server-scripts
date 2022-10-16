7601d7147ef909151e95a1ea122ca338c4395a7c
Pro4TLZZ <github@pro4tlzz.com>
2022-10-16 22:01:08 +0100

    Update overseerrSyncDeletedPlexMedia.js
    Add bad pagination logic

--- overseerrSyncDeletedPlexMedia.js
+++ overseerrSyncDeletedPlexMedia.js
@@ -5,12 +5,31 @@ async function  getMe() {
     console.log(res);
 }
 
-async function listMedia() {
-    const url = '/api/v1/media';
+async function listMedia(mediaList) {
+    const limit = 20;
+    var skip = 20;
+    const url = '/api/v1/media?take=' + limit ;
     const r = await fetch (url, {method: 'get'});
     const res = await r.json();
     console.log(res);
+    var results = res.results;
+    mediaList.push(results);
+    console.table(results);
+    const pageInfo = res.pageInfo;
+    const page = pageInfo.page;
+    const pages = pageInfo.pages;
+
+    if (page != pages) {
+        skip += 20;
+        const url = '/api/v1/media?take=' + limit + '&skip=' + skip ;
+        console.log(url);
+        const r = await fetch (url, {method: 'get'});
+        const res = await r.json();
+        console.log(res);
+    }
+
 }
 
+var mediaList = [];
 await getMe();
-await listMedia();
+await listMedia(mediaList);
